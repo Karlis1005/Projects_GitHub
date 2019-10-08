04b3d827307dc82b39cc38eb919af6cdd5809297
Karlis1005 <56017098+Karlis1005@users.noreply.github.com>
2019-10-08 09:31:10 +0200

    Improved file1 further

--- file1.py
+++ file1.py
@@ -1,3 +1,6 @@
+import math
+def funcsqrt():
+    print(math.sqrt(x))
 def funcdiv():
     print(x / y)
 def funcmult():
@@ -9,8 +12,8 @@ def funccube():
 def funcadd():
     print(x + y)
 def funcsubtract():
-    print(x - y):
-function_choice = input("What do you want to do? Write sqr, cube, add, subtract, division or multiplication: ")
+    print(x - y)
+function_choice = input("What do you want to do? Write sqrt, sqr, cube, add, subtract, division or multiplication: ")
 if function_choice == "division":
     valuex = input("What will your first number be? ")
     valuey = input("Your second number? ")
@@ -23,6 +26,10 @@ elif function_choice == "multiplication":
     x = int(valuex)
     y = int(valuey)
     funcmult()
+elif function_choice == "sqrt":
+    valuex = input("What will your number be? ")
+    x = int(valuex)
+    funcsqrt()
 elif function_choice == "sqr":
     valuex = input("What will your number be? ")
     x = int(valuex)
@@ -31,7 +38,7 @@ elif function_choice == "cube":
     valuex = input("What will your number be? ")
     x = int(valuex)
     funccube()
-elif function_choice == "add"
+elif function_choice == "add":
     valuex = input("What will your first number be? ")
     valuey = input("Your second number? ")
     x = int(valuex)
@@ -47,4 +54,3 @@ else:
     print("Sorry, wrong input!")
 dontuse = input("press Enter to close")
 # This is a calculator.
-# This line is meant just for rounding up the amount of lines.
